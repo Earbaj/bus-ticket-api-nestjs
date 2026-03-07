f83ce489075f7299e57679e921f370641bb963d2
Earbaj <earbajsaria3@gmail.com>
2026-03-07 13:36:35 +0600

    ADD: auth,users,seats,buses,notification,payment,module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BusesModule } from './buses/buses.module';
+import { SeatsModule } from './seats/seats.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { PaymentModule } from './payment/payment.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI!)
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    AuthModule,
+    UsersModule,
+    BusesModule,
+    SeatsModule,
+    TicketsModule,
+    PaymentModule,
+    NotificationsModule
   ],
   controllers: [AppController],
   providers: [AppService],
