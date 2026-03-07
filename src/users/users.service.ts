913bccbd988762064d2224c55c17e8abdb15d99d
Earbaj <earbajsaria3@gmail.com>
2026-03-07 13:46:16 +0600

    add methode to service class where all add delete update read methode for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+import { User, UserDocument } from './users.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+
+    const existingUser = await this.userModel.findOne({
+      email: createUserDto.email,
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const user = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return user.save();
+  }
+
+  async findAll() {
+    return this.userModel.find().select('-password');
+  }
+
+  async findOne(id: string) {
+    return this.userModel.findById(id).select('-password');
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(
+      id,
+      updateUserDto,
+      { new: true },
+    ).select('-password');
+  }
+
+  async remove(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+
+}
